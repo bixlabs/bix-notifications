f954421d8aa4f4ebcaa0acd563e56820a17580bf
Edward Suárez <eddsuarez89@gmail.com>
2017-03-06 17:09:10 -0300

    Build before publish
    - es6 format in index.js and config.js
    - added /dist folder to .gitignore
    - added /src folder to .npmignore
    - added npm script `prepublish`

--- src/index.js
+++ src/index.js
@@ -1,86 +1,110 @@
-const db = require('./notifications/db');
-const Configuration = require('./notifications/config');
-const notificationController = require('./notifications/controller');
+import db from './notifications/db';
+import Configuration from './notifications/config';
+import notificationController from './notifications/controller';
 
-function Notification(config) {
-  this.config = new Configuration(config);
-  this.configDatabase = this.config.database;
-  this.baseApi = this.config.baseApi;
+class Notification {
+  constructor(config) {
+    this.config = new Configuration(config);
+    this.configDatabase = this.config.database;
+    this.baseApi = this.config.baseApi;
+  }
+
+  /**
+   * This method will be modified!
+   */
+  startServer(app) {
+    const self = this;
+    self.app = app;
+    self.io = app.io;
+    db.connect(this.configDatabase);
+
+    // Expose endpoints
+    self.app.get(`${self.baseApi}/all/:userid`, notificationController.getAll);
+    self.app.put(`${self.baseApi}/:notificationId`, notificationController.updateUnread);
+    self.app.delete(`${self.baseApi}/:notificationId`, notificationController.delete);
+    self.app.post(`${self.baseApi}/all`, notificationController.updateAllUnread);
+
+    self.io.on('connection', (socket) => {
+      socket.emit('who-are-you');
+
+      socket.on('check-in', (user, ack) => {
+        self.io.sockets[user.id] = socket;
+        self.sendUnread(user.id);
+
+        // Send to client endpoint base route forexposed endpoints
+        ack(`{"baseApi":"${self.baseApi}"}`);
+      });
+
+      socket.on('check-news', (user) => {
+        self.sendUnsent(user.id);
+      });
+    });
+  }
+
+  /**
+   * Send notification using socket
+   * @param  {string} event        custom socket event
+   * @param  {object} notification object representation for notification
+   */
+  send(event, notification) {
+    const user = notification.to;
+    this.io.sockets[user].emit(event, notification, (ack) => {
+      if (ack) {
+        notificationController
+        .update(notification._id, { sent: true }, //eslint-disable-line
+          (err) => {
+            if (err) throw err;
+          });
+      }
+    });
+  }
+
+  /**
+   * Sends all the unread notifications by user id
+   * @param  {string} userId user identifier
+   */
+  sendUnread(userId) {
+    const self = this;
+    if (userId) {
+      notificationController
+      .getUnread(userId, (err, data) => {
+        if (err) throw err;
+        data.forEach((notification) => {
+          self.send('unread', notification);
+        });
+      });
+    }
+  }
+
+  /**
+   * Sends all unsent notifications by user id-
+   * @param  {string} userId User identifier
+   */
+  sendUnsent(userId) {
+    const self = this;
+    if (userId) {
+      notificationController
+      .getUnsent(userId, (err, data) => {
+        if (err) throw err;
+        data.forEach((notification) => {
+          self.send('news', notification);
+        });
+      });
+    }
+  }
+
+  /**
+   * Register a new notification in the database
+   * @param  {string} data.type  type of the notification
+   * @param  {string} data.to    user indentifier
+   * @param  {string} data.title title fo the notification
+   * @param  {string} data.msg   notification content
+   */
+  static create(data) {
+    notificationController.create(data, (err) => {
+      if (err) throw err;
+    });
+  }
 }
 
-Notification.prototype.startServer = (app) => {
-  const self = this;
-  self.app = app;
-  self.io = app.io;
-  db.connect(this.configDatabase);
-
-  // Expose endpoints
-  self.app.get(`${self.baseApi}/all/:userid`, notificationController.getAll);
-  self.app.put(`${self.baseApi}/:notificationId`, notificationController.updateUnread);
-  self.app.delete(`${self.baseApi}/:notificationId`, notificationController.delete);
-  self.app.post(`${self.baseApi}/all`, notificationController.updateAllUnread);
-
-  self.io.on('connection', (socket) => {
-    socket.emit('who-are-you');
-
-    socket.on('check-in', (user, ack) => {
-      self.io.sockets[user.id] = socket;
-      self.sendUnread(user.id);
-
-      // Send to client endpoint base route forexposed endpoints
-      ack(`{"baseApi":"${self.baseApi}"}`);
-    });
-
-    socket.on('check-news', (user) => {
-      self.sendUnsent(user.id);
-    });
-  });
-};
-
-Notification.prototype.send = (event, notification) => {
-  const user = notification.to;
-  this.io.sockets[user].emit(event, notification, (ack) => {
-    if (ack) {
-      notificationController
-      .update(notification._id, { sent: true }, //eslint-disable-line
-        (err) => {
-          if (err) throw err;
-        });
-    }
-  });
-};
-
-Notification.prototype.sendUnread = (userId) => {
-  const self = this;
-  if (userId) {
-    notificationController
-    .getUnread(userId, (err, data) => {
-      if (err) throw err;
-      data.forEach((notification) => {
-        self.send('unread', notification);
-      });
-    });
-  }
-};
-
-Notification.prototype.sendUnsent = (userId) => {
-  const self = this;
-  if (userId) {
-    notificationController
-    .getUnsent(userId, (err, data) => {
-      if (err) throw err;
-      data.forEach((notification) => {
-        self.send('news', notification);
-      });
-    });
-  }
-};
-
-Notification.prototype.create = (type, to, title, msg) => {
-  const data = { to, type, title, msg };
-  notificationController.create(data, (err) => {
-    if (err) throw err;
-  });
-};
-
-module.exports = Notification;
+export default Notification;
--- src/notifications/config.js
+++ src/notifications/config.js
@@ -1,42 +1,43 @@
-const path = require('path');
-const fs = require('fs');
+import path from 'path';
+import fs from 'fs';
 
-/**
- * Construct a configuration object.
- *
- * A configuration object may be constructed with either
- * a path to a `notification.json` file (which defaults to
- * `$PWD/notification.json` if not present, or with a configuration
- * object.
- *
- * @param {String|Object} config Configuration path or details.
- *
- * @constructor
- */
-function Configuration(config) {
-  if (!config) {
-    // I placed the disable line here, however we must not reassign
-    // parameters, this is not allowed, we must fix it later.
-    config = path.join(process.cwd(), 'notification.json'); //eslint-disable-line
+class Configuration {
+  /**
+   * Construct a configuration object.
+   *
+   * A configuration object may be constructed with either
+   * a path to a `notification.json` file (which defaults to
+   * `$PWD/notification.json` if not present, or with a configuration
+   * object.
+   *
+   * @param {String|Object} config Configuration path or details.
+   *
+   * @constructor
+   */
+  constructor(config) {
+    if (!config) {
+      // I placed the disable line here, however we must not reassign
+      // parameters, this is not allowed, we must fix it later.
+      config = path.join(process.cwd(), 'notification.json'); //eslint-disable-line
+    }
+
+    if (typeof config === 'string') {
+      this.loadConfiguration(config);
+    } else {
+      this.configure(config);
+    }
   }
 
-  if (typeof config === 'string') {
-    this.loadConfiguration(config);
-  } else {
+  loadConfiguration(configPath) {
+    const jsonConfig = fs.readFileSync(configPath, 'utf8');
+    const config = JSON.parse(jsonConfig.toString());
     this.configure(config);
+  }
+
+  configure(config) {
+    this.database = config.database;
+    this.baseApi = config.baseApi;
   }
 }
 
-Configuration.prototype.loadConfiguration = function loadConfiguration(configPath) {
-  const json = fs.readFileSync(configPath);
-  const config = JSON.parse(json.toString());
-  this.configure(config);
-};
-
-
-Configuration.prototype.configure = function configure(config) {
-  this.database = config.database;
-  this.baseApi = config.baseApi;
-};
-
-module.exports = Configuration;
+export default Configuration;
