1c32f6a091fb951c7601cb93be65ed92664bd8b8
Joseph Arrieta <jac.wshmstr@gmail.com>
2017-03-10 18:34:54 -0500

    added Debug mode to the Module
    remove the environment variable for the debug file, added some utilities to use callback or promises inside of the controller, I haven't test this I will do it whenever the testing branch is merged.
    
    we don't use notification attributes because we don't know what attributes the notification model will have.

--- src/notifications/controller.js
+++ src/notifications/controller.js
@@ -1,3 +1,6 @@
+import Debug from './debug';
+import {tryCallbackOrPromise, tryCallbackOrThrowError} from './util';
+
 class NotificationController {
   db;
 
@@ -7,74 +10,143 @@ class NotificationController {
    */
   constructor(notificationDatabase) {
     this.db = notificationDatabase;
+    Debug.info('Notifications were initialized');
   }
 
   /**
    * Creates a new notification.
    * @param  {Object} data, object representing a notification.
+   * @param  {Function} callback
    */
-  create(data) {
-    return this.db.create(data);
+  async create(data, callback = null) {
+    try {
+      const notification = await this.db.create(data);
+      Debug.info(`Notification was created at ${new Date()} with values: ${JSON.stringify(notification)}`);
+      return tryCallbackOrPromise(notification, callback);
+    } catch (error) {
+      Debug.error(`An error was thrown while trying to create a Notification: ${error}`);
+      tryCallbackOrThrowError(error, callback);
+    }
   }
 
   /**
    * Updates a notification by id.
    * @param  {String} id
    * @param  {Object} data, values to update
+   * @param  {Function} callback
    */
-  update(id, data) {
-    return this.db.update(id, data);
+  async update(id, data, callback = null) {
+    try {
+      const notification = await this.db.update(id, data);
+      Debug.info(`Notification was updated at ${new Date()} with values: ${JSON.stringify(notification)}`);
+      return tryCallbackOrPromise(notification, callback);
+    } catch (error) {
+      Debug.error(`An error was thrown while trying to update a Notification: ${id} -- ${error}`);
+      tryCallbackOrThrowError(error, callback);
+    }
   }
 
   /**
    * Gets all notifications by user id.
    * @param {String} userId
+   * @param {Function} callback
    */
-  findAllByUser(userId) {
-    return this.db.findAllByUser(userId);
+  async findAllByUser(userId, callback = null) {
+    try {
+      const notifications = await this.db.findAllByUser(userId);
+      Debug.info(`All Notifications for user: ${userId} were retrieved at ${new Date()}`);
+      return tryCallbackOrPromise(notifications, callback);
+    } catch (error) {
+      Debug.error(`An error was thrown while trying to find all Notifications of a user: ${userId} -- ${error}`);
+      tryCallbackOrThrowError(error, callback);
+    }
   }
 
   /**
    * Sets unread field to false, meaning that the
    * notification is read.
    * @param {String} id
+   * @param {Function} callback
    */
-  setAsRead(id) {
-    return this.db.setAsRead(id);
+  async setAsRead(id, callback = null) {
+    try {
+      const notification = await this.db.setAsRead(id);
+      Debug.info(`Notification with id: ${id} was read at ${new Date()}`);
+      return tryCallbackOrPromise(notification, callback);
+    } catch (error) {
+      Debug.error(`An error was thrown while trying to set as read a Notification: ${id} -- ${error}`);
+      tryCallbackOrThrowError(error, callback);
+    }
   }
 
   /**
    * Sets all unread notifications as read,
    * basically, sets unread field to be false.
    * @param {String} userId
+   * @param {Function} callback
    */
-  setAllAsRead(userId) {
-    return this.db.setAllAsRead(userId);
+  async setAllAsRead(userId, callback = null) {
+    try {
+      const notifications = await this.db.setAllAsRead(userId);
+      Debug.info(`All Notifications for user: ${userId} were set as read at ${new Date()}`);
+      return tryCallbackOrPromise(notifications, callback);
+    } catch (error) {
+      Debug.error(`An error was thrown while trying to set all Notifications as read of user: ${userId} -- ${error}`);
+      tryCallbackOrThrowError(error, callback);
+    }
   }
 
   /**
    * Removes a notification by id.
    * @param {String} id
+   * @param {Function} callback
    */
-  remove(id) {
-    return this.db.remove(id);
+  async remove(id, callback = null) {
+    try {
+      const notification = await this.db.remove(id);
+      Debug.info(`Notification with id: ${id} was removed at ${new Date()}`);
+      return tryCallbackOrPromise(notification, callback);
+    } catch (error) {
+      Debug.error(`An error was thrown while trying to remove a Notification: ${id} -- ${error}`);
+      tryCallbackOrThrowError(error, callback);
+    }
   }
 
   /**
    * Gets all unread notifications of a user.
    * @param  {String} userId
+   * @param  {Function} callback
    */
-  getAllUnread(userId) {
-    return this.db.getAllUnread(userId);
+  async getAllUnread(userId, callback = null) {
+    try {
+      const notifications = await this.db.getAllUnread(userId);
+      Debug.info(`Unread Notifications for user: ${userId} were retrieved at ${new Date()}`);
+      return tryCallbackOrPromise(notifications, callback);
+    } catch (error) {
+      Debug.error(`An error was thrown while trying to get all unread 
+        Notifications of a user: ${userId} as read -- ${error}`);
+      tryCallbackOrThrowError(error, callback);
+    }
   }
 
   /**
    * Gets all unsent notifications of a user.
    * @param  {String} userId User id
+   * @param  {Function} callback
    */
-  getAllUnsent(userId) {
-    return this.db.getAllUnsent(userId);
+  async getAllUnsent(userId, callback = null) {
+    try {
+      const notifications = await this.db.getAllUnsent(userId);
+      Debug.info(`Unsent Notifications for user: ${userId} were retrieved at ${new Date()}`);
+      return tryCallbackOrPromise(notifications, callback);
+    } catch (error) {
+      Debug.error(`An error was thrown while trying to get all unsent 
+        Notifications of a user: ${userId} -- ${error}`);
+      tryCallbackOrThrowError(error, callback);
+    }
   }
+
+
 }
 
 export default NotificationController;
