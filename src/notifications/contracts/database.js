1638a99189f99b8f9864f731bda2c0cfca28343f
Joseph Arrieta <jac.wshmstr@gmail.com>
2017-04-04 15:05:37 -0500

    just starting to create the documenation of how to use the library, remember that we will probably have adapters to make it easier to use this.

--- src/notifications/contracts/database.js
+++ src/notifications/contracts/database.js
@@ -1,20 +1,53 @@
 class NotificationDatabaseContract {
   /* eslint-disable */
 
-  create(data) {}
+  /**
+   * @param notification
+   * @return {Promise} which resolves to the Object representing the notification that was created.
+   */
+  create(notification) {}
 
-  update(id, data) {}
+  /**
+   * @param id
+   * @param notification
+   * @return {Promise} which resolves to an Object representing the notification that was updated.
+   */
+  update(id, notification) {}
 
+  /**
+   * @param id
+   * @return {Promise} which resolves to an Object representing the notification that was removed.
+   */
   remove(id) {}
 
+  /**
+   * @param id
+   * @return {Promise} which resolves to an Object representing the notification that was change to read.
+   */
   setAsRead(id) {}
 
+  /**
+   * @param userId
+   * @return {Promise} which resolves to an Array with all the notifications that were set as read for the given user id.
+   */
   setAllAsRead(userId) {}
 
+  /**
+   * @param userId
+   * @return {Promise} which resolves to an Array with all the notifications that are unread for the given user id.
+   */
   getAllUnread(userId) {}
 
+  /**
+   * @param userId
+   * @return {Promise} which resolves to an Array with all the notifications that are unsent for the given user id.
+   */
   getAllUnsent(userId) {}
 
+  /**
+   * @param userId
+   * @return {Promise} which resolves to an Array with all the notifications for the given user id.
+   */
   findAllByUser(userId) {}
 }
 
